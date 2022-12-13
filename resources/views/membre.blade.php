d28e0f5737e420281b0f9e97fd0b4775f725f6c5
samakem <119296259+samakem@users.noreply.github.com>
2022-12-13 23:49:24 +0000

    Add files via upload
    le nom de la view de la page d'acceauil c'est welcome, celle de la presentation des membres s'appelle membre

---
+++ resources/views/membre.blade.php
@@ -0,0 +1,295 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="\frontend\style.css">
+    <title>Les membres du groupe 5</title>
+</head>
+<body>
+    
+<h1>Ce projet à été realisé par</h1>
+
+
+
+    <style>
+      .box {
+    padding-bottom: 5px;
+    border: 0px black solid;
+    border-radius: 10px;
+  
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+    
+    display: block;
+    flex-wrap: wrap; 
+    justify-content: space-between;
+    display:inline-block;
+    margin: 8%;
+
+      
+      }
+      .box-inner {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        text-align: center;
+        transition: transform 0.5s;
+        transform-style: preserve-3d;
+        width: 300px;
+        height: 366px;
+        margin-bottom: 10px;
+
+      }
+      .box:hover .box-inner {
+        transform: rotateY(180deg);
+      }
+      .box-front,
+      .box-back {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        backface-visibility: hidden;
+        min-width: 200px;
+         height: 300px;  
+        margin-bottom: 30px;
+        /* margin-right: 5% */
+        
+
+      }
+      .box-front {
+        background-color: #cccccc;
+        color: white;
+      }
+      .box-back {
+        /* background-color: #8ebf42; */
+        color: #eeeeee;
+        transform: rotateY(180deg);
+      }
+    </style>
+  </head>
+  <body>
+    <section>
+
+
+
+
+
+
+
+
+       
+    <div id="Membre"></div>
+    <div class="box">
+      <div class="box-inner">
+        <div class="box-front">
+           <h2>Assitan S Traoré</h2> 
+           <div class="datenom">
+            <span > Etudiante en 3ISR</span>
+        </div>
+        </div>
+        <div class="box-back">
+            <div class="classe">
+                <a href="#">
+                    <div class="Pictures">
+                        <img src="frontend\Pictures\AT.jpg" alt="frontend\Pictures\AT.jpg" srcset="frontend\Pictures\AT.jpg">
+                    </div>
+       
+                <div class="datenom">
+                <span >Assitan S TRaoré</span>
+                <span > Etudiante en 3ISR</span>
+            </div>
+             </a>
+           </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div id="Membre"></div>
+    <div class="box">
+      <div class="box-inner">
+        <div class="box-front">
+           <h2>Ousmane Fané</h2> 
+           <div class="datenom">
+            <span > Etudiant en 3ISR</span>
+        </div>
+        </div>
+        <div class="box-back">
+            <div class="classe">
+                <a href="#">
+                    <div class="Pictures">
+                        <img src="frontend\Pictures\OF.jpg" alt="frontend\Pictures\OF.jpg" srcset="frontend\Pictures\OF.jpg">
+                    </div>
+       
+                <div class="datenom">
+                <span >Ousmane Fané</span>
+                <span > Etudiant en 3ISR</span>
+            </div>
+             </a>
+           </div>
+        </div>
+      </div>
+    </div>
+
+    
+    <div id="Membre"></div>
+    <div class="box">
+      <div class="box-inner">
+        <div class="box-front">
+           <h2>Boubacar Sid LY</h2> 
+           <div class="datenom">
+            <span > Etudiant en 3ISR</span>
+        </div>
+        </div>
+        <div class="box-back">
+            <div class="classe">
+                <a href="#">
+                    <div class="Pictures">
+                        <img src="frontend\Pictures\BSL.jpg" alt="frontend\Pictures\BSL.jpg" srcset="frontend\Pictures\BSL.jpg">
+                    </div>
+       
+                <div class="datenom">
+                <span >Boubacar Sid Ly</span>
+                <span > Etudiant en 3ISR</span>
+            </div>
+             </a>
+           </div>
+        </div>
+      </div>
+    </div>
+   
+
+<div id="Membre"></div>
+    <div class="box">
+      <div class="box-inner">
+        <div class="box-front">
+           <h2>Mariam Samaké</h2> 
+           <div class="datenom">
+            <span > Etudiante en 3ISR</span>
+        </div>
+        </div>
+        <div class="box-back">
+            <div class="classe">
+                <a href="#">
+                    <div class="Pictures">
+                        <img src="frontend\Pictures\MS.jpg" alt="frontend\Pictures\MS.jpg" srcset="frontend\Pictures\MS.jpg">
+                    </div>
+       
+                <div class="datenom">
+                <span >Mariam Samaké</span>
+                <span > Etudiante en 3ISR</span>
+            </div>
+             </a>
+           </div>
+        </div>
+      </div>
+    </div>
+
+    
+    <div id="Membre"></div>
+    <div class="box">
+      <div class="box-inner">
+        <div class="box-front">
+           <h2>Fatoumata D Bathily</h2> 
+           <div class="datenom">
+            <span > Etudiante en 3ISR</span>
+        </div>
+        </div>
+        <div class="box-back">
+            <div class="classe">
+                <a href="#">
+                    <div class="Pictures">
+                        <img src="frontend\Pictures\ASB.jpg" alt="frontend\Pictures\ASB.jpg" srcset="frontend\Pictures\ASB.jpg">
+                    </div>
+       
+                <div class="datenom">
+                <span >Fatoumata D Bathily</span>
+                <span > Etudiante en 3ISR</span>
+            </div>
+             </a>
+           </div>
+        </div>
+      </div>
+    </div>
+
+    <div id="Membre"></div>
+    <div class="box">
+      <div class="box-inner">
+        <div class="box-front">
+           <h2>Sory I Fofana</h2> 
+           <div class="datenom">
+            <span > Etudiant en 3ISR</span>
+        </div>
+        </div>
+        <div class="box-back">
+            <div class="classe">
+                <a href="#">
+                    <div class="Pictures">
+                        <img src="frontend\Pictures\SF.jpg" alt="frontend\Pictures\SF.jpg" srcset="frontend\Pictures\SF.jpg">
+                    </div>
+       
+                <div class="datenom">
+                <span >Sory I Fofana</span>
+                <span > Etudiant en 3ISR</span>
+            </div>
+             </a>
+           </div>
+        </div>
+      </div>
+    </div>
+
+    <div id="Membre"></div>
+    <div class="box">
+      <div class="box-inner">
+        <div class="box-front">
+           <h2>Mandani Wagué</h2> 
+           <div class="datenom">
+            <span > Etudiant en 3ISR</span>
+        </div>
+        </div>
+        <div class="box-back">
+            <div class="classe">
+                <a href="#">
+                    <div class="Pictures">
+                        <img src="frontend\Pictures\MW.jpg" alt="frontend\Pictures\MW.jpg" srcset="frontend\Pictures\MW.jpg">
+                    </div>
+       
+                <div class="datenom">
+                <span >Madani Wagué</span>
+                <span > Etudiant en 3ISR</span>
+            </div>
+             </a>
+           </div>
+        </div>
+      </div>
+    </div>
+
+    <div id="Membre"></div>
+    <div class="box">
+      <div class="box-inner">
+        <div class="box-front">
+           <h2>Salimata M Keita</h2> 
+           <div class="datenom">
+            <span > Etudiante en 3ISR</span>
+        </div>
+        </div>
+        <div class="box-back">
+            <div class="classe">
+                <a href="#">
+                    <div class="Pictures">
+                        <img src="frontend\Pictures\LK.jpg" alt="frontend\Pictures\LK.jpg" srcset="frontend\Pictures\LK.jpg">
+                    </div>
+       
+                <div class="datenom">
+                <span >Salimata M kéita</span>
+                <span > Etudiante en 3ISR</span>
+            </div>
+             </a>
+           </div>
+        </div>
+      </div>
+    </div>
+
+</section>
+
---
+++ storage/framework/views/50bb4b0aed4fbdd69f979e0fac0cb532baed21d5.php
@@ -0,0 +1,37 @@
+
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                <link rel="stylesheet" href="\frontend\style.css">
+                <title>G5 website</title>
+            </head>
+            <body>
+                
+            
+            <!-- <button  class="connexion"> <a href="/login">Connexion</a> </button>  <button class="Confidentialité"> <a href="#"> Confidentialité </a> </button> -->
+            
+             <nav class="navbar">
+                
+                <ul> 
+                     <li><a href="#"> Acceuil</a></li>  
+                     <li><a href="#">A propros</a></li>
+                     <li><a href="#">Service</a></li>      <input type="search" placeholder="Recherche" class="search">
+                </ul>
+               
+              
+             </nav>
+             <div class="img">
+                <img src="\frontend\Pictures\R (2).jpeg" alt="\frontend\Pictures\R (2).jpeg" srcset="\frontend\Pictures\R (2).jpeg" width="100%" >
+            </div>
+           
+                <h1> <span class="bienvenue" >  Bienvenue</span> <br> sur le site officiel <span class="G5">du groupe 5</span> </h1>
+                <div class="btn">
+
+                <a href="/membre">   <button class="bt"> Cliquez-ici</button></a>
+            </div>
+            </body>
+            </html>
+<?php /**PATH C:\Users\USER\Desktop\Groupe5\resources\views/welcome.blade.php ENDPATH**/ ?>
